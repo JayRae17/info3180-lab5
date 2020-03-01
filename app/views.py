e47f2d33e09d9af37d38e8818926f1b0a2549832
Jada-Rae Brown <kourtney_jada@hotmail.com>
2020-03-01 13:27:16 -0500

    'update_loginRoute'

--- app/views.py
+++ app/views.py
@@ -11,6 +11,8 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
 
+from werkzeug.security import generate_password_hash
+
 
 ###
 # Routing for your application.
@@ -34,7 +36,10 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
+            user = UserProfile.query.filter_by(username=username,password=password).first()
             # Get the username and password values from the form.
 
             # using your model, query database for a user based on the username
@@ -44,10 +49,11 @@ def login():
             # passed to the login_user() method below.
 
             # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            if user is not None and check_password_hash(user.password,password):
+                login_user(user)
+                 # remember to flash a message to the user
+                flash('Logged in successfully.', 'success')
+                return redirect(url_for('secure-page')) # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
