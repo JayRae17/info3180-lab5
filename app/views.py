8038573f76ed695bd160d9bdfbb86f287553189b
Jada-Rae Brown <kourtney_jada@hotmail.com>
2020-03-01 15:42:35 -0500

    'final'

--- app/views.py
+++ app/views.py
@@ -35,39 +35,50 @@ def secure_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    #if current_user.is_authenticated:
-        # if user is already logged in, just redirect them to  secure page
-        #return redirect(url_for('secure_page'))
-
+    if current_user.is_authenticated:
+        return redirect(url_for('secure_page'))
+ 
     form = LoginForm()
     # change this to actually validate the entire form submission
     # and not just one field
     if request.method == 'POST' and form.validate_on_submit():
-       # Get the username and password values from the form.
+        # Get the username and password values from the form.
         username = form.username.data
         password = form.password.data
-
         # using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+            # and password submitted. Remember you need to compare the password hash.
+            # You will need to import the appropriate function to do so.
+            # Then store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method below.
 
         user = UserProfile.query.filter_by(username=username).first()
+
         if user is not None and check_password_hash(user.password, password):
+            remember_me = False
+            if 'remember_me' in request.form:
+                remember_me = True
 
-            # If the user is not blank, meaning if a user was actually found,
-            # then login the user and create the user session.
-            # user should be an instance of your `User` class
-
-            # get user id, load into session
+            login_user(user, remember=remember_me)
 
             # remember to flash a message to the user
             flash('Logged in successfully.', 'success')
-            return redirect(url_for('secure_page')) # they should be redirected to a secure-page route instead
+
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('secure_page')) # they should be redirected to a secure-page route instead
+
         else:
+
             flash('Username or Password is incorrect.', 'danger')
+
     return render_template('login.html', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    # end the session
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
 
 
 
